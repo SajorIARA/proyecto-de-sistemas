5fa1687e08ec036c142a2f7cb6f6657e66600fb1
SajorIARA <sajorignacio@gmail.com>
2026-09-18 02:42:00 -0400

    fix: ACCESS_TOKEN_LIFETIME 30min to match regression test

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,7 +131,7 @@ REST_FRAMEWORK = {
 
 # Simple JWT
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "USER_ID_FIELD": "id_usuario",
     "USER_ID_CLAIM": "user_id",
